98ca1a0d3d8792a065747b59852a8bd368ccdb10
superbia01 <73924786+superbia01@users.noreply.github.com>
2020-11-09 19:19:30 +0700

    Update register.php

--- login & logout/register.php
+++ login & logout/register.php
@@ -1,11 +1,13 @@
 <?php
-$name = $_POST['name'];
+$first_name = $_POST['frist_name'];
+$last_name = $_POST['last_name'];
 $email = $_POST['email'];
+$phone_number = $_POST['phone_number'];
 $username = $_POST['username'];
 $passwd = $_POST['passwd'];
 require_once('connect.php');
-$q="INSERT INTO user (name,email,username,passwd,disable)
-VALUES ('".$name."','".$email."','".$username."','".$passwd."',0)";
+$q="INSERT INTO user (frist_name,last_name,phone_number,email,username,passwd,disable)
+VALUES ('".$first_name."','".$last_name."','".$email."','".$phone_number."','".$username."','".$passwd."',0)";
 $result = $mysqli->query($q);
 if (!$result) {
 die('Error: '.$q." ". $mysqli->error);
