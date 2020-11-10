3cbd48448b8a1b342040154e03cbe49c797b7e9c
mkmmook <73730897+mkmmook@users.noreply.github.com>
2020-11-10 23:01:41 +0700

    1

---
+++ staff/delinfo.php
@@ -0,0 +1,15 @@
+<?php
+	// $g_id = $_GET['id'];
+	$id = $_GET['id'];
+	require_once('connect.php');
+	if (isset($id)) {
+		$q="DELETE FROM movie where m_id=$id";
+			if(!$mysqli->query($q)){
+				echo "DELETE failed. Error: ".$mysqli->error ;
+		   }
+		   $mysqli->close();
+		   //redirect
+		   header("Location: editmovie.php");
+	}
+	
+?>
